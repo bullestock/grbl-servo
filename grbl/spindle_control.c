2462515605988125a25e18a8850103f7b40963d7
Torsten Martinsen <torsten@bullestock.net>
2022-09-11 20:55:49 +0200

    Apply changes from https://github.com/cprezzi/grbl-servo/commit/fb5f6ae130a50a693432b71fca56ff0df8e9acf9

--- grbl/spindle_control.c
+++ grbl/spindle_control.c
@@ -192,15 +192,27 @@ void spindle_stop()
     uint8_t spindle_compute_pwm_value(float rpm) // 328p PWM register is 8-bit.
     {
       uint8_t pwm_value;
+#ifndef SPINDLE_IS_SERVO
       rpm *= (0.010*sys.spindle_speed_ovr); // Scale by spindle speed override value.
+#endif
       // Calculate PWM register value based on rpm max/min settings and programmed rpm.
       if ((settings.rpm_min >= settings.rpm_max) || (rpm >= settings.rpm_max)) {
         // No PWM range possible. Set simple on/off spindle control pin state.
         sys.spindle_speed = settings.rpm_max;
         pwm_value = SPINDLE_PWM_MAX_VALUE;
       } else if (rpm <= settings.rpm_min) {
-          sys.spindle_speed = settings.rpm_min;
-          pwm_value = SPINDLE_PWM_MIN_VALUE;
+          if (rpm == 0.0) { // S0 disables spindle
+#ifndef SPINDLE_IS_SERVO
+              sys.spindle_speed = 0.0;
+              pwm_value = SPINDLE_PWM_OFF_VALUE;
+#else
+              sys.spindle_speed = settings.rpm_min;
+              pwm_value = SPINDLE_PWM_MIN_VALUE;
+#endif
+          } else { // Set minimum PWM output
+              sys.spindle_speed = settings.rpm_min;
+              pwm_value = SPINDLE_PWM_MIN_VALUE;
+          }
       } else { 
         // Compute intermediate PWM value with linear spindle speed model.
         // NOTE: A nonlinear model could be installed here, if required, but keep it VERY light-weight.
@@ -228,10 +240,17 @@ void spindle_stop()
   if (state == SPINDLE_DISABLE) { // Halt or set spindle direction and rpm.
   
     #ifdef VARIABLE_SPINDLE
+      #ifndef SPINDLE_IS_SERVO
       sys.spindle_speed = 0.0;
-    #endif
+      spindle_stop();
+      #else
+      // For servo send min. PWM instead of deactivate PWM
+      sys.spindle_speed = SPINDLE_PWM_MIN_VALUE;
+      spindle_set_speed(spindle_compute_pwm_value(SPINDLE_PWM_MIN_VALUE));      
+      #endif
+    #else
     spindle_stop();
-  
+    #endif  
   } else {
     
     #if !defined(USE_SPINDLE_DIR_AS_ENABLE_PIN) && !defined(ENABLE_DUAL_AXIS)
